72a616e4d3fa0b520b3beb6ceaf56ed296ee2425
Sandro Ciervo <sandro.ciervo@neolution.ch>
2025-03-17 17:40:09 +0100

    Add CultureInfo extension method to get language code

---
+++ Neolution.Utilities/Globalization/CultureInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Neolution.Utilities.Globalization
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// Extension methods for <see cref="CultureInfo"/>.
+    /// </summary>
+    public static class CultureInfoExtensions
+    {
+        /// <summary>
+        /// Gets the language code
+        /// </summary>
+        /// <param name="cultureInfo">The culture information</param>
+        /// <returns>The language code</returns>
+        public static string GetLanguageCode(this CultureInfo cultureInfo)
+        {
+            ArgumentNullException.ThrowIfNull(cultureInfo);
+            return cultureInfo.IsNeutralCulture ? cultureInfo.Name : cultureInfo.Parent.Name;
+        }
+    }
+}
